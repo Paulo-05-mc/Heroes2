b5326f569026f598904dc19f60a5357149edbd42
Paulo <a2022212002@estudiantes.upsa.edu.bo>
2025-03-17 20:05:16 -0400

    Genere Controlador CRUD

--- Heroes/Data/AppDbContext.cs
+++ Heroes/Data/AppDbContext.cs
@@ -1,6 +1,9 @@
-﻿namespace Heroes.Data;
+﻿using Heroes.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace Heroes.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<Heroe> Heroes { get; set; }
 }
--- Heroes/Models/Heroe.cs
+++ Heroes/Models/Heroe.cs
@@ -2,5 +2,9 @@
 
 public class Heroe
 {
-    
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string HeroeName { get; set; } = null!;
+    public int Age { get; set; }
 }
